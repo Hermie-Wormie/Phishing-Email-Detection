00d4070570a1da60c7d41dc02422a6c9114ef539
aq1608 <shuangxrong08@gmail.com>
2024-09-19 18:01:36 +0800

    edited main

--- main.py
+++ main.py
@@ -11,12 +11,17 @@ def extract_data():
     datasets: list[Path] = []
     datasets.extend(dataset_dir.glob(ext))
 
+    sep_data: list[Path] = []
+    sep_data.extend(cleandata_dir.glob(ext))
+
     status = cleandata_dir.exists()
     if status:
         # print(f"Removing existing {cleandata_dir} ... ", end="")
         shutil.rmtree(cleandata_dir)
     # print(f"Making {cleandata_dir} ... ", end="")
     cleandata_dir.mkdir()
+    for path in sep_data:
+        path.touch()
 
     return datasets
 

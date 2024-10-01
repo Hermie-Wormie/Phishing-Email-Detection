e683890689b5187013b55f1a3690273f5268a142
Hermie-Wormie <lwjHerman@outlook.com>
2024-10-01 19:00:45 +0800

    Updated code to return permalink of both url and hash scan results

--- VirusTotal.py
+++ VirusTotal.py
@@ -5,6 +5,7 @@ from email.parser import Parser
 
 API_KEY = 'c177ebfe27c37c0aec97609d333dcf803645c65fc855f44d5ffebfd8993ff700'
 VT_URL = 'https://www.virustotal.com/vtapi/v2/url/report'
+VT_FILE = 'https://www.virustotal.com/vtapi/v2/file/report'
 
 
 def file_to_hash(file_path):
@@ -19,41 +20,33 @@ def file_to_hash(file_path):
 
 
 def check_hash(file_hash):
-    url = f"https://www.virustotal.com/vtapi/v2/file/report"
-    params = {
-        'apikey': API_KEY,
-        'resource': file_hash
-    }
-    response = requests.get(url, params=params)
+    params = {'apikey': API_KEY, 'resource': file_hash}
+    response = requests.get(VT_FILE, params=params)
+
+    # Check if response is successful
     if response.status_code == 200:
         result = response.json()
         if result:
             return hash_scan_results(result)
-        
+
         else:
             return "Empty response from VirusTotal."
     else:
-        return f"Error: {response.status_code}"
+        return f"Error: {response.status_code} from VirusTotal"
 
 
 def hash_scan_results(result):
     if result['response_code'] == 1:
-        # print(f"Hash: {result['resource']}")
 
         if result['positives'] > 0:
-            numPositives = result['positives']
-            print(numPositives)
-            return numPositives
-            # print(f"Malicious! Positives: {result['positives']}")
-            # print(f"Vendors that flagged this file as malicious:")
+            num_positives = result['positives']
+            vt_link = result['permalink']
+            return num_positives, vt_link
 
-            # for vendor, report in result['scans'].items():
-            #     if report['detected']:
-            #         print(f"     - {vendor}: {report['result']}")
         else:
             return "The file is clean."
     else:
-        return "Hash not found in VirusTotal database."
+        return "Hash of file not found in VirusTotal database."
 
 
 def check_url(url):
@@ -78,32 +71,27 @@ def url_scan_results(result):
 
     if result['response_code'] == 1:
         if result['positives'] > 0:
-            numPositive = result['positives']
-            return numPositive
-        #     print(f"Vendors that flagged this site as malicious:")
-        #     for vendor, report in result['scans'].items():
-        #         if report['detected']:
-        #             print(f"     - {vendor}: {report['result']}")
-        # else:
-        #     print("Clean URL")
+            num_positive = result['positives']
+            vt_link = result['permalink']
+            return num_positive, vt_link
+
         else:
             return "Clean URL"
+
     else:
         return "URL not found in VirusTotal database"
 
 
 if __name__ == "__main__":
-
-    # URL and Filepath of attachment
+    
+    # Test malicious url: 
     url = "http://www.myetherevvalliet.com/"
-    file_path = r"C:\Users\herma\OneDrive\Desktop\hi.jpg"
-
-    # Check URL on VirusTotal
     # print(check_url(url))
 
-    # Test malicious hash: b1b74ff5c67cfdc16f1cb30db9b885046c4c5d71af575a2d3de4a79918c1ce89
-    # hash_result = check_hash("b1b74ff5c67cfdc16f1cb30db9b885046c4c5d71af575a2d3de4a79918c1ce89")
+    # Test malicious hash: 
+    hash = "b1b74ff5c67cfdc16f1cb30db9b885046c4c5d71af575a2d3de4a79918c1ce89"
+    # print(check_hash(hash))
 
     # Hash file then check VirusTotal
     # hash_of_file = file_to_hash(file_path)
-    # check_hash("b1b74ff5c67cfdc16f1cb30db9b885046c4c5d71af575a2d3de4a79918c1ce89")
+    # print(check_hash(hash_of_file))
